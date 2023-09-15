64bd187760d501ba86793598900ac4616c04e3dd
Lud0do1202 <ludo.traina@gmail.com>
2023-09-15 20:44:17 +0200

    POST topic

--- topic.php
+++ topic.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $ez = new EZQuery();
 
     // Get all topics from user
-    $topics = $ez->executeSelect("SELECT id, topic, color FROM topics WHERE id_user = '$userID'");
+    $topics = $ez->executeSelect("SELECT id, topic, theme FROM topics WHERE id_user = '$userID'");
 
     /* ------------------------------ Response ------------------------------ */
     http_response_code(200);  // OK
@@ -33,10 +33,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     exit;
 }
 
+
+/* ========================================================================== */
+/*                                    POST                                    */
+/* ========================================================================== */ //
+else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    /* ----------------------------- Get User ID ---------------------------- */
+    $userID = $_SERVER['HTTP_X_USER_ID'] ?? null;
+
+    /* ------------------------------ Forbidden ----------------------------- */
+    if ($userID === null) {
+        http_response_code(403);
+        exit;
+    }
+
+    /* ------------------------------ Get Topic ----------------------------- */
+    $data = json_decode(file_get_contents("php://input"));
+    $topic = $data->topic->topic;
+    $theme = $data->topic->theme;
+
+    /* ------------------------------- EZQuery ------------------------------ */
+    $ez = new EZQuery();
+
+    // Insert new user if it doesn't exist
+    $rowsAffected = $ez->executeEdit("INSERT INTO topics (id_user, topic, theme) VALUES ('$userID', '$topic', '$theme')");
+
+    /* ------------------------------ Response ------------------------------ */
+    switch ($rowsAffected) {
+        case 1: // SUCCESS
+            // Get last id insert
+            $lastID = $ez->executeSelect("SELECT LAST_INSERT_ID() as lastID");
+
+            // Update the topic that was given
+            $data->topic->id = $lastID[0]['lastID'];
+
+            // Response
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($data->topic);
+            exit;
+
+        default: // Unknow error
+            http_response_code(520);
+            exit;
+    }
+}
+
 /* ========================================================================== */
 /*                                     PUT                                    */
 /* ========================================================================== */ //
-if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     /* ----------------------------- Get User ID ---------------------------- */
     $userID = $_SERVER['HTTP_X_USER_ID'] ?? null;
 
@@ -50,23 +96,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     $data = json_decode(file_get_contents("php://input"));
     $id = $data->topic->id;
     $topic = $data->topic->topic;
-    $color = $data->topic->color;
+    $theme = $data->topic->theme;
 
     /* ------------------------------- EZQuery ------------------------------ */
     $ez = new EZQuery();
 
     // Insert new user if it doesn't exist
-    $rowsAffected = $ez->executeEdit("UPDATE topics SET topic = '$topic', color = '$color' WHERE id = '$id'");
+    $rowsAffected = $ez->executeEdit("UPDATE topics SET topic = '$topic', theme = '$theme' WHERE id = '$id'");
 
     /* ------------------------------ Response ------------------------------ */
     switch ($rowsAffected) {
         case 0: // Nothing updated
         case 1: // SUCCESS
-            http_response_code(204);
-            break;
+            http_response_code(200);
+            header('Content-Type: application/json');
+            echo json_encode($data->topic);
+            exit;
 
         default: // Unknow error
             http_response_code(520);
-            break;
+            exit;
     }
 }
